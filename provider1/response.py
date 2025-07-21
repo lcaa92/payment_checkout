fe2866d8b72ff5888c353dc927599fb481617748
Leandro Araujo <leandrocaaraujo@gmail.com>
2025-07-21 20:38:32 -0300

    Creating responses classes

---
+++ provider1/response.py
@@ -0,0 +1,24 @@
+import uuid
+from pydantic import BaseModel
+
+class ChargeResponse(BaseModel):
+    id: uuid.UUID
+    createdAt: str
+    status: str
+    originalAmount: int
+    currentAmount: int
+    currency: str 
+    description: str
+    paymentMethod: str 
+    cardId: uuid.UUID
+    
+class RefundResponse(BaseModel):
+    id: uuid.UUID
+    created_at: str
+    status: str
+    original_amount: int
+    current_amount: int
+    currency: str
+    description: str
+    payment_method: str
+    card_id: uuid.UUID
